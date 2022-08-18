d6f7542ac56672311acace977887354aedab9fc8
luyosamson <luyosamson@gmail.com>
2022-08-18 08:38:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Your code here
+const dodger = document.querySelector("#dodger");
+const moveDodgerLeft = () => (dodger.style.left = "50px");
+const moveDodgerRight = () => (dodger.style.left = "250px");
